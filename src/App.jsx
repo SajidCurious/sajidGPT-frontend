2b0f71b1491fcca13f61da7164a69dca768ee2eb
Mohammed <sajidcurious9@gmail.com>
2023-10-17 19:59:28 +0530

    added section and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 function App() {
-  return <>App</>;
+  return (
+    <main className="chatGPT-app">
+      <section className="chat-container">
+        <div className="layout">
+          <div className="chat-bubble"></div>
+        </div>
+      </section>
+      <footer>
+        <input
+          className="composebar"
+          autoFocus
+          type="text"
+          placeholder="Ask Anything"
+          onChange={() => {}}
+        />
+        <div className="send-button">
+          <img src={send} />
+        </div>
+      </footer>
+    </main>
+  );
 }
 
 export default App;
