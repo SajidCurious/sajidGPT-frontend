a4a139e1e61c2b03d50ee03b554ef33f187d01fd
Mohammed <sajidcurious9@gmail.com>
2023-10-17 20:32:20 +0530

    addding layout

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,18 @@ function App() {
     <main className="chatGPT-app">
       <section className="chat-container">
         <div className="layout">
-          <div className="chat-bubble"></div>
+          <div className="chat-bubble">
+            <div className="avatar">
+              <img src={user} />
+            </div>
+            <div className="post">Lorem ipsum dolor sit amet.</div>
+          </div>
+          <div className="chat-bubble bot">
+            <div className="avatar">
+              <img src={bot} />
+            </div>
+            <div className="post">Lorem ipsum dolor sit amet.</div>
+          </div>
         </div>
       </section>
       <footer>
